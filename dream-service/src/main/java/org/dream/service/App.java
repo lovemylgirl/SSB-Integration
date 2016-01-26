a23430448c95468a26766299d2d36deb8882e147
murphys <murphys.tian@gmail.com>
2016-01-26 09:53:48 +0800

    first commit

---
+++ dream-service/src/main/java/org/dream/service/App.java
@@ -0,0 +1,13 @@
+package org.dream.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

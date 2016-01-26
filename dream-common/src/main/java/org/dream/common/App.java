a6fd500356317c8a59c7bf01271cac2121e29f35
murphys <murphys.tian@gmail.com>
2016-01-26 10:09:57 +0800

    调整代码格式

--- dream-common/src/main/java/org/dream/common/App.java
+++ dream-common/src/main/java/org/dream/common/App.java
@@ -4,10 +4,8 @@ package org.dream.common;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
